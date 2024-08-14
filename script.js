fac15bac08aaa511df86e3d0cd19612f310fd424
BellaAkins <sarabellaakins@gmail.com>
2024-08-13 17:52:16 -0700

    add script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ close.addEventListener("click", () => {
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "images/sun.svg";
+    icon.src = "images/sun-icon.svg";
   } else {
     icon.src = "images/moon.svg";
   }
